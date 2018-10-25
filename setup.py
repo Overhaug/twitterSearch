e12801ac337da1809a84004aacf5941f02fd6df3
Sebastian Øverhaug <Overhaug15@gmail.com>
2018-10-25 10:56:29 +0200

    2nd..

---
+++ setup.py
@@ -0,0 +1,33 @@
+import tweepy
+import sys
+from configs import config
+
+# sys.path.append("..")
+
+auth = tweepy.OAuthHandler(config.apiKey, config.secretKey)
+auth.set_access_token(config.token, config.secretToken)
+
+api = tweepy.API(auth)
+
+
+# inherits from streamListener
+# overrides on_status
+class CustomStreamListener(tweepy.StreamListener):
+    def on_status(self, status):
+        print(status.text, status.geo)
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            # False disconnects the stream
+            return False
+
+        # if True, reconnects the stream
+
+
+thisStreamListener = CustomStreamListener()
+thisStream = tweepy.Stream(auth=api.auth, listener=thisStreamListener)
+
+for tweet in tweepy.Cursor(api.search, q="*", count=5, geocode="60.3652817,5.2890912,50km").items(5):
+    print(tweet.created_at, "|", tweet.user.name, "|", tweet.text, "|", tweet.place.name if tweet.place else "Undefined place")
+
+
