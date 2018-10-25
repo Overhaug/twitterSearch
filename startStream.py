5ddfd8f9d0abf72f11f18604040c3a56d16a29bd
Sebastian Øverhaug <Overhaug15@gmail.com>
2018-10-25 15:55:36 +0200

    nm

--- startStream.py
+++ startStream.py
@@ -1,8 +1,7 @@
 import tweepy
-import sys
+import csv
 from configs import config
 
-# sys.path.append("..")
 
 auth = tweepy.OAuthHandler(config.apiKey, config.secretKey)
 auth.set_access_token(config.token, config.secretToken)
@@ -14,7 +13,7 @@ api = tweepy.API(auth)
 # overrides on_status
 class CustomStreamListener(tweepy.StreamListener):
     def on_status(self, status):
-        print(status.text, status.geo)
+        print(status.text)
 
     def on_error(self, status_code):
         if status_code == 420:
@@ -24,10 +23,24 @@ class CustomStreamListener(tweepy.StreamListener):
         # if True, reconnects the stream
 
 
+def write_data():
+    with open('data/data.csv', mode="w", encoding="utf-8") as twitter_data:
+        data_writer = csv.writer(twitter_data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        data_writer.writerow(result)
+
+
+def define_location():
+    for tweet in tweepy.Cursor(api.search, q="*", count=5, geocode="60.3652817,5.2890912,50km").items(5):
+        print(tweet.created_at, "|", tweet.user.name, "|", tweet.text, "|",
+              tweet.place.name if tweet.place else "Undefined place")
+        r = tweet.created_at, tweet.user.name, tweet.text, tweet.place.name if tweet.place else "Undefined place"
+        result.append(r)
+    write_data()
+
+
 thisStreamListener = CustomStreamListener()
 thisStream = tweepy.Stream(auth=api.auth, listener=thisStreamListener)
 
-for tweet in tweepy.Cursor(api.search, q="*", count=5, geocode="60.3652817,5.2890912,50km").items(5):
-    print(tweet.created_at, "|", tweet.user.name, "|", tweet.text, "|", tweet.place.name if tweet.place else "Undefined place")
+result = []
 
-
+define_location()
