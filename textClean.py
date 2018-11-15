42c0b2a12e66c24960aae1dbf7771fb6b0f778c2
DESKTOP-834203O\Viktor <vikhan91@gmail.com>
2018-11-15 03:58:19 +0100

    textClean.py åpner en json i en dataframe, og gir muligheten til å lete etter hashtags eller keywords. Forså å spytte ut en ny json fil som bare inneholde de tweetsene med gitt premis. sentimentAnalysis, er en mulig start på den, men den blir ikke brukt til noe enda.

--- textClean.py
+++ textClean.py
@@ -5,23 +5,26 @@ import nltk
 import json
 from pandas.io.json import json_normalize
 
-file = "data/json_data.json"
-df = pd.read_json(file, orient='columns')
+# load json object
+with open("data/json_data.json", encoding="utf-8") as f:
+    d = json.load(f)
+
+# Creates a dataframe; each row is a tweet, each column is a tweet attribute + tweet ID
+df = json_normalize(data=d['tweets'], record_path='tweet',
+                            meta=['tweet_id'])
 
 df.columns
-df.head(10)
-texts = df["tweet_text"].head(10)
-texts.head(10)
-
-
-
+df.head(5)
+df['tweet_text'].head()
 #_____________ TextCleaning__________________
 # Convert to lowercase
 df['tweet_text'] = df['tweet_text'].apply(lambda x: " ".join(x.lower() for x in x.split()))
 df['tweet_text'].head()
 
 # Remove punctuation
-df['tweet_text'] = df['tweet_text'].str.replace('[^\w\s]','')
+#rgx = '[^\w\s]'  # remove any word or whitespace character, this includes #
+rgx = '[.,]'  # remove , or .
+df['tweet_text'] = df['tweet_text'].str.replace(rgx, '')
 df['tweet_text'].head()
 
 
@@ -36,18 +39,19 @@ keyword = "trump"
 df['keyword'] = df['tweet_text'].apply(lambda x: len([x for x in x.split() if x.startswith(keyword)]))
 df[['keyword']].head()
 
+
 keywordDf = df[(df['keyword'] >= 1)]
 keywordDf["keyword"].head()
 keywordDf["keyword"].max()
-
-keyword_json = keywordDf.to_json(orient='values')
+keywordDf.head()
+keyword_json = keywordDf.to_json(orient='records')
 
 
 def dump_json2(tweets, file):
     with open(file, "w") as write_file:
         json.dump(tweets, write_file, indent=4)
 
-dump_json2(keyword_json, "data/keyword_json.json")
+dump_json2(keyword_json, "data/keyword_data.json")
 
 
 
@@ -56,22 +60,22 @@ dump_json2(keyword_json, "data/keyword_json.json")
 # Remove stopwords
 #from nltk.corpus import stopwords
 #stop = stopwords.words('english')
-#train['tweet'] = train['tweet'].apply(lambda x: " ".join(x for x in x.split() if x not in stop))
-#train['tweet'].head()
+#df['tweet_text'] = df['tweet_text'].apply(lambda x: " ".join(x for x in x.split() if x not in stop))
+#df['tweet_text'].head()
 
 # Remove common words
-freq = pd.Series(' '.join(df['tweet_text']).split()).value_counts()[:10]
+#freq = pd.Series(' '.join(df['tweet_text']).split()).value_counts()[:10]
 
 # Remove rare words
-freq = pd.Series(' '.join(df['tweet_text']).split()).value_counts()[-10:]
-freq
+#freq = pd.Series(' '.join(df['tweet_text']).split()).value_counts()[-10:]
+#freq
 
 # Library to combine words that are not spelled correct.
-from textblob import TextBlob
-df['tweet_text'][:5].apply(lambda x: str(TextBlob(x).correct()))
+#from textblob import TextBlob
+#df['tweet_text'][:5].apply(lambda x: str(TextBlob(x).correct()))
 
 
-df["tweet_text"].head(100)
+#df["tweet_text"].head(100)
 
 # Stemming, removing suffices, brave and bravly is looked at as the same.
 #from nltk.stem import PorterStemmer
@@ -79,17 +83,17 @@ df["tweet_text"].head(100)
 #df['tweet_text'][:5].apply(lambda x: " ".join([st.stem(word) for word in x.split()]))
 
 # Lemmatization, Tries to reduce the words to its root word, rather then drop the ending of it.
-from textblob import Word
+#from textblob import Word
 #nltk.download("wordnet")
 
-df['tweet_text'] = df['tweet_text'].apply(lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
-df['tweet_text'].head(10)
+#df['tweet_text'] = df['tweet_text'].apply(lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
+#df['tweet_text'].head(10)
 
 # Tokenize word list
-from nltk.tokenize import TweetTokenizer
-tknzr = TweetTokenizer(strip_handles=True, reduce_len=True)
-joinedTweets = ''.join(df["tweet_text"])
-tknzr.tokenize(joinedTweets)
+#from nltk.tokenize import TweetTokenizer
+#tknzr = TweetTokenizer(strip_handles=True, reduce_len=True)
+#joinedTweets = ''.join(df["tweet_text"])
+#tknzr.tokenize(joinedTweets)
 
-df["tweet_text"][0]
+#df["tweet_text"][0]
 
